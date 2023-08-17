1021557a0aace74bd19f33d735762a8de934403f
dipsambhavani <dip.sambhavani@gamil.com>
2023-08-17 19:16:52 +0530

    added daily quote widget

--- app/src/main/java/com/example/wordwise/DailyQuote.java
+++ app/src/main/java/com/example/wordwise/DailyQuote.java
@@ -3,7 +3,18 @@ package com.example.wordwise;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +24,25 @@ public class DailyQuote extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.daily_quote);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, "https://api.quotable.io/quotes/random", null, response -> {
+            try {
+                JSONObject object = response.getJSONObject(0);
+                views.setTextViewText(R.id.quote_of_the_day, object.getString("content").trim());
+                views.setTextViewText(R.id.author_name, "~" + object.getString("author").trim() + "~");
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(appWidgetId, views);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }, error -> Toast.makeText(context, "api call error!!!", Toast.LENGTH_SHORT).show());
+
+        queue.add(jsonArrayRequest);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
